f97a3cbd254aa39bd6bb1fa926030c2059819cdf
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-03-10 21:11:48 +0530

    this is my commit

--- Basic1/index3.ts
+++ Basic1/index3.ts
@@ -1,11 +1,17 @@
 //Regular func
-function addone(num: number){
-    return num+1;
-
+function addone(num: number) {
+  return num + 1;
 }
 console.log(addone(3));
 
 //Arrow Function Annotations
-const double = (x: number, y: number)=>x*y;
-const res= double(2,10);
+const double = (x: number, y: number) => x * y;
+const res1 = double(2, 10);
+console.log(res1);
+//Default Params Values
+const greet = (person: string = "Anonymous") => {
+  return `Hello ${person}`;
+};
+const res = greet();
 console.log(res);
+2
