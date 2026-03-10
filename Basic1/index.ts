b28df1177ae0ad8a3b3676ef585593588157a02b
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-03-10 20:45:52 +0530

    this is my initial commit

---
+++ Basic1/index.ts
@@ -0,0 +1,16 @@
+//Strings
+let myName: string = "Gaurav Jain";
+myName = "Gaurav Kumar Borad";
+console.log(myName);
+//Numbers
+let favNumber: number = 8;
+console.log(favNumber);
+//Boolean
+let tsHard: boolean = false;
+tsHard = true;
+console.log(tsHard);
+// here we're inferring the types!
+let tech = "TypeScript";
+let favNumber1 = 8;
+let isWeak = true;
+console.log(tech, favNumber1, isWeak);
