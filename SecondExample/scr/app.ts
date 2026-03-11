c963f0ada604f46ebefc3f181f8c5ffa0d52e9e6
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-03-11 21:48:16 +0530

    this is my primitive data type

---
+++ SecondExample/scr/app.ts
@@ -0,0 +1,11 @@
+var name:string = "garuav jain";
+console.log(name);
+var num: number = 10;
+console.log(num);
+var num: number = 10.2;
+console.log(num);
+var male: boolean = true;
+console.log(male);
+var data= null;
+var a:undefined= undefined;
+console.log(a)
