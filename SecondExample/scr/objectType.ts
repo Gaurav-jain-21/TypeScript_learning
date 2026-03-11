4cde75c1f8185f55715329332936961f64a62f6f
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-03-11 21:52:31 +0530

    this is my object data type

---
+++ SecondExample/scr/objectType.ts
@@ -0,0 +1,8 @@
+let numbers: number[] = [1, 2, 3, 4]; //list
+let names: Array<string> = ["Garuav", "jain", "Vikash", "Rahul"]; //Array
+console.log(numbers);
+console.log(names);
+let person: [string, number] = ["Gaurav", 21]; //tuple
+console.log(person);
+let user: { name: string; age: number } = { name: "Gaurav", age: 22 }; //objects
+console.log(user);
