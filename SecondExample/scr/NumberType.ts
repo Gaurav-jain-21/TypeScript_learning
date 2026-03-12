1e4ce985146045942cdf230ed21e39d541974ca2
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-03-12 20:46:35 +0530

    this is my Number type

---
+++ SecondExample/scr/NumberType.ts
@@ -0,0 +1,17 @@
+var num1: number =10;
+let num2: number=10;
+let num3: number=30
+var total:number= num1+num3;
+console.log(total);
+var oct: number=0o00001;//you have to use  0o before any value for octal
+var hexa: number=0x0001;//you have to use 0x before any value for hexa
+var binary: number=0b0001;// you have to use 0b before any value for binary
+console.log(oct+10);
+console.log(hexa+10);
+console.log(binary+10);
+
+var item:number=100;
+var item2="50";
+var item2converted= Number(item2);
+console.log(item+item2converted);
+
