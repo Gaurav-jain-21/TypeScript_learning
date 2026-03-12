3de7877f606305f3f8496395a9597592230bfd77
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-03-12 20:58:09 +0530

    this about my string and boolean data type

---
+++ SecondExample/scr/StringType.ts
@@ -0,0 +1,19 @@
+var str:string="Gaurav jain";
+var age: number =22;
+var userName: string = "Garuav Jain";
+var info: string=`my name is ${userName} and age is ${age} years`
+console.log(info);
+var num: number=100;
+var data:string= num.toString();
+console.log(data);
+var data:string = ""+num;
+console.log(data);
+var boolData=true;
+var data: string= boolData.toString();
+console.log(data);
+
+var item: boolean= true;
+var other: boolean;
+other=false;
+console.log(other);
+var info1 = "Gaurav is a good boy";
