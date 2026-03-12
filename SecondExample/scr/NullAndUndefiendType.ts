31318951dafd5b4b4367369d0bfffc002b177f87
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-03-12 21:11:44 +0530

    this is my null data type

---
+++ SecondExample/scr/NullAndUndefiendType.ts
@@ -0,0 +1,24 @@
+//the variable is empty that is null data type; or absense of the value in a variable
+//when we create a variable but not asscy the value with it that time the variable is undefined
+var data: null=null;
+var value:undefined= undefined;
+
+
+var item: null | string= null;
+item="Gaurav"
+console.log(item);
+var userName:null | string=null;
+var login=true;
+if(login){
+    userName="Gaurav Jain";
+}
+console.log(userName);
+
+
+console.log(typeof data);
+
+
+
+var item1:undefined|string=undefined;
+item1="we dont talk anymore like we use to do";
+console.log(typeof item1);
