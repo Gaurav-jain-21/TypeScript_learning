9ca7ba24a9a933859cc2bb4ce793dde352f09594
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-03-13 21:28:27 +0530

    this is example of array

---
+++ SecondExample/scr/ArrayType.ts
@@ -0,0 +1,7 @@
+// Collection of same data type
+// pre-Defined data type
+
+let numbers: number[] = [1, 2, 3, 4, 5];
+let names: Array<string> = ["Gaurav", "Vikash", "Rahul", "Ashif"];
+console.log(numbers);
+console.log(names);
