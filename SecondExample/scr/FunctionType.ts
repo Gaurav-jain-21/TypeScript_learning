8fe38ab4516792566c57377f5207f336acbf5469
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-03-21 23:17:17 +0530

    this is my function

---
+++ SecondExample/scr/FunctionType.ts
@@ -0,0 +1,15 @@
+function fruits(): string {
+  return "Apple";
+}
+console.log(fruits());
+function simple(name:string):boolean{
+    if(name==="Gaurav"){
+        console.log(name);
+        return true;
+    }
+    else{
+        return false;
+    }
+    // return true;
+}
+console.log(simple("abhisake"));
