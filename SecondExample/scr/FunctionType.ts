7db54df0fdb0afb742335a1673ad39c525e57a7a
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-03-21 23:19:18 +0530

    this is my commit

--- SecondExample/scr/FunctionType.ts
+++ SecondExample/scr/FunctionType.ts
@@ -2,14 +2,13 @@ function fruits(): string {
   return "Apple";
 }
 console.log(fruits());
-function simple(name:string):boolean{
-    if(name==="Gaurav"){
-        console.log(name);
-        return true;
-    }
-    else{
-        return false;
-    }
-    // return true;
+function simple(name: string): boolean | number {
+  if (name === "Gaurav") {
+    console.log(name);
+    return true;
+  } else {
+    return false;
+  }
+  // return true;
 }
 console.log(simple("abhisake"));
