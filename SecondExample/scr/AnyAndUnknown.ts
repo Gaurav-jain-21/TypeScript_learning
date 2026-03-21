19d6bf6077a2784563264f49c3c257a3aa29aab0
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-03-21 22:45:36 +0530

    any and unkonwn

---
+++ SecondExample/scr/AnyAndUnknown.ts
@@ -0,0 +1,10 @@
+// Any:- it allows a variable to hold values of any type.
+var value= "Anil";
+console.log(typeof(value));
+
+var values:any="Ram";
+console.log(typeof(values))
+values=435;
+console.log(typeof(values))
+values=true;
+console.log(typeof(values))
