a62bd27203866d6213f391110114362f87ea5bff
Gaurav jain <gjain7764@gmail.com>
2026-09-01 13:52:31 +0530

    this is my commit

---
+++ Basics1/example5.ts
@@ -0,0 +1,14 @@
+const numbers : number[]=[1,23,5,3,8];
+const names: Array<string>=["Gaurav","jain","john"]
+console.log(numbers)
+
+const items: string[]=[];
+items.push("KeyBorad")
+items.push("mouse")
+const product=[]
+product.push("garuav")
+product.push(12)
+product.push(true)
+
+const matrix: number[][]=[[12,42,64,875],[467,743,349,93]]
+console.log(matrix)
