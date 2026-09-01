d71db471b6eb956e47fff8f30def7ff9d33a7cad
Gaurav jain <gjain7764@gmail.com>
2026-09-01 13:26:20 +0530

    this is my commit

---
+++ Basics1/example1.ts
@@ -0,0 +1,10 @@
+//Strings
+let myName: string = "gaurav";
+myName = "Somethings other persone";
+console.log(myName);
+//Numbers
+let favNumber: number = 8;
+console.log(favNumber);
+//boolean
+let isLoged: boolean = false;
+console.log(isLoged);
